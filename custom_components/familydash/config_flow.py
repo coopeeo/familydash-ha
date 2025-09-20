0e433b2c39bdedf639d253127fe6904abb610171
Cooper <coop@coopeeo.xyz>
2025-09-20 17:45:20 -0500

    testing

---
+++ custom_components/familydash/config_flow.py
@@ -0,0 +1,19 @@
+import voluptuous as vol
+
+from homeassistant import config_entries
+
+from .const import DOMAIN
+
+
+class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Example config flow."""
+
+    VERSION = 1
+
+    async def async_step_user(self, info):
+        if info is not None:
+            pass  # TODO: process info
+
+        return self.async_show_form(
+            step_id="user", data_schema=vol.Schema({vol.Required("password"): str})
+        )
