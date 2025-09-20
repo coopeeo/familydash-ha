87d5b1779fe2bec1b6cc7ba1a26c28fa17b49efb
Cooper <coop@coopeeo.xyz>
2025-09-20 16:43:38 -0500

    linting and uv rahhh

--- custom_components/familydash/__init__.py
+++ custom_components/familydash/__init__.py
@@ -1,7 +1,7 @@
 from .const import DOMAIN
 
+
 async def async_setup(hass, config):
-    """Set up this component using YAML is not supported."""
     hass.states.async_set(f"{DOMAIN}.hello_world", "Hello World!")
 
     return True
