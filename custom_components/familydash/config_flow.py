3e7e6280b52f8039b122b3568757cea7e48fff15
Cooper <coop@coopeeo.xyz>
2025-09-22 18:56:58 +0000

    try stuff iguess

--- custom_components/familydash/config_flow.py
+++ custom_components/familydash/config_flow.py
@@ -1,9 +1,33 @@
+from mimetypes import init
+import voluptuous as vol
+
 from homeassistant import config_entries
 
 from .const import DOMAIN
 
 
-class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Example config flow."""
+class FamilyDashConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """FamilyDash config flow."""
 
     VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> config_entries.ConfigFlowResult:
+        if user_input is not None:
+            return self.async_step_link(user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required("host"): str,
+                    vol.Required("port"): int,
+                }
+            ),
+        )
+
+    async def async_step_link(
+        self, user_input: dict[str, Any] | None = None
+    ) -> config_entries.ConfigFlowResult:
+        return self.async_show_form(step_id="link")
