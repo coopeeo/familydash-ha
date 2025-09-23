0bb3962f4d4f37cad7f5d23d2c730da2ca0ec1fc
Cooper <coop@coopeeo.xyz>
2025-09-23 15:09:59 +0000

    aaaa

--- custom_components/familydash/config_flow.py
+++ custom_components/familydash/config_flow.py
@@ -30,7 +30,7 @@ class FamilyDashConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self,
         user_input: dict[str, any] | None = None,  # type: ignore  # noqa: PGH003
     ) -> config_entries.ConfigFlowResult:
-        if user_input is None:
+        if user_input is not None:
             return self.async_show_form(step_id="link")
 
         return self.async_show_progress(step_id="link", progress_action="Loading")
